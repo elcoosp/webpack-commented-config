77589f9d8646bf92f7215ab51dea440d716e23d6
Elcoosp <elcoosp@gmail.com>
2018-04-04 22:15:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# webpack-commented-config
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin') // Npm installed
+const webpack = require('webpack') //For built-in plugins
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  // Main js file, shortand for entry: { main: ... }, we can pass an arry instead of main
+  // We could also specify vendors to split the libraries apart and cache them on the client
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js', // Referenced in the public/index.html file, use [name] if multiple entry points
+    path: path.resolve(__dirname, 'dist') // Where to write the bundle
+  },
+  mode: 'development', // Or development, set process.env.NODE_ENV
+  module: {
+    rules: [
+      // Set rules of loaders to use for the tested type of files, loaders are "piped"
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin(),
+    new HtmlWebpackPlugin({ template: './src/public/index.html' })
+  ]
+}
