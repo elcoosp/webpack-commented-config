04fde91dbf9ca9a4b579a66bf2bbb67c336fccc8
Elcoosp <elcoosp@gmail.com>
2018-04-04 23:06:56 +0200

    Target prop

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   // Main js file, shortand for entry: { main: ... }, we can pass an arry instead of main
   // We could also specify vendors to split the libraries apart and cache them on the client
   entry: './src/index.js',
+  target: 'web', // Specify the environnment in which the programm is running (node, web by default)
   output: {
     filename: 'bundle.js', // Referenced in the public/index.html file, use [name] if multiple entry points
     path: path.resolve(__dirname, 'dist') // Where to write the bundle
